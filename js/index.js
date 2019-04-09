d2da7c5af45ee23ecb64b5d9d1393b3f3c5a6679
azrap <azraap@gmail.com>
2019-04-09 15:40:58 -0700

    added the code for stopping event propogation to the click events for button and navigation - MVP

--- js/index.js
+++ js/index.js
@@ -17,24 +17,53 @@
  const btn=document.querySelector(".btn");
  console.log(btn);
  
- //click
+ // 1.click
  btn.addEventListener('click', (event) => {
     btn.style.backgroundColor = "blue";
+    eventObject.stopPropogation(); //stopping the propogation since we have 2 click events
   })
 
-// mouseover x
+// 2. mouseover x
   btn.addEventListener('mouseover', (event) => {
     btn.style.backgroundColor = "yellow";
     btn.style.color = "black";
   })
 
-  // dblclick x
+  // 3.dblclick x
   btn.addEventListener('dblclick', (event) => {
     btn.style.backgroundColor = "orange";
     btn.style.color = "purple";
   })
 
-  //load
+  //4.load
   window.addEventListener('load', (event) => {
     console.log('page is fully loaded');
 });
+
+//5.keydown
+window.addEventListener('keydown', (event) => {
+    console.log('some one pressed a key!');
+});
+
+// 6. need to add better code here for 3
+//select 
+window.addEventListener('select', (event) => {
+    console.log('some one pressed a key!');
+});
+
+//7.
+
+//8.
+
+//9.
+
+//10.
+
+
+// stopping an event from propogating:
+const navigation=querySelector(".nav");
+addEventListener('click', (event) => {
+    navigation.style.backgroundColor = "green";
+    eventObject.stopPropogation();
+  })
+// on line 20 we have another click event, so we need to stop them fom propogating to each other.
