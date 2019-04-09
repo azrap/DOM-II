5bd54eac1da68bc7adf8dc8faa66c33807ac42c4
azrap <azraap@gmail.com>
2019-04-09 15:20:16 -0700

    added 5 out of 10 of the events - MVP task 1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
 // Your code goes here
+
+
+
+/* 
+ keydown 
+ wheel 
+ drag / drop
+ load
+ focus
+ resize
+ scroll
+ select
+ 
+ */
+
+ const btn=document.querySelector(".btn");
+ console.log(btn);
+ 
+ //click
+ btn.addEventListener('click', (event) => {
+    btn.style.backgroundColor = "blue";
+  })
+
+// mouseover x
+  btn.addEventListener('mouseover', (event) => {
+    btn.style.backgroundColor = "yellow";
+    btn.style.color = "black";
+  })
+
+  // dblclick x
+  btn.addEventListener('dblclick', (event) => {
+    btn.style.backgroundColor = "orange";
+    btn.style.color = "purple";
+  })
+
+  //load
+  window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+});
